9295c7a806e11e078b81e53265070a91e1d08119
Akshara <aksharar02@gmail.com>
2025-01-23 09:32:54 +0530

    Setup basic Node.js project with Express, Dotenv, Mongoose, and Nodemon

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const app =express();
-const port =3000;
-app.get('/',(req,res)=>{
-    res.send('Hello world')
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
 });
-app.listen(port,()=>{
-    console.log(`Server running at http://localhost:${port}`)
-})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
